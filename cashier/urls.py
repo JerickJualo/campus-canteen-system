6330987b3d25fb33bdedce9ebbcfd9707bc3bfea
ELAINE JANE <elaineilola8@gmail.com>
2026-05-02 21:32:29 +0800

    sales report

--- cashier/urls.py
+++ cashier/urls.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponse
 from django.urls import path
+from . import views
+
 from .views import (
     cashier_home, 
     add_to_cart, 
@@ -15,4 +18,9 @@ urlpatterns = [
     path('increase/<int:item_id>/', increase_quantity, name='increase_quantity'),
     path('decrease/<int:item_id>/', decrease_quantity, name='decrease_quantity'),
     path('checkout/', checkout, name='checkout'),
+    path('reports/daily/', views.daily_report, name='daily_report'),
+    path('reports/monthly/', views.monthly_report, name='monthly_report'),
+    path('reports/daily/delete/', views.delete_daily_report, name='delete_daily_report'),
+    path('reports/monthly/delete/', views.delete_monthly_report, name='delete_monthly_report'),
+   path('daily/cashier/', views.daily_report, name='cashier_home')
 ]
